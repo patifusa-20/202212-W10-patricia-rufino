40e800c207d8d3d257bf640e0c5900393aded930
patridifusa <p.rufino.a@gmail.com>
2022-12-12 16:54:43 +0000

    Render data in cards

--- src/features/home/components/gentleman/gentleman.tsx
+++ src/features/home/components/gentleman/gentleman.tsx
@@ -1,13 +1,20 @@
+import { DataGentlemanType } from '../../../../core/types/gentleman';
+import { gentlemenData } from '../../../../core/mocks/gentlemen.data';
 import { Button } from '../button/button';
 import { Info } from '../info/info';
 
 export function Gentleman() {
+    const items = gentlemenData;
     return (
         <>
-            <li className="gentleman">
-                <Info></Info>
-                <Button></Button>
-            </li>
+            {items.map((item: DataGentlemanType) => {
+                return (
+                    <li className="gentleman">
+                        <Info item={item}></Info>
+                        <Button></Button>
+                    </li>
+                );
+            })}
         </>
     );
 }
--- src/features/home/components/info/info.tsx
+++ src/features/home/components/info/info.tsx
@@ -1,22 +1,24 @@
-export function Info() {
+import { DataGentlemanType } from '../../../../core/types/gentleman';
+
+export function Info({ item }: { item: DataGentlemanType }) {
     return (
         <>
             <div className="gentleman__avatar-container">
                 <img
                     className="gentleman__avatar"
-                    src="assets/img/fary.jpg"
-                    alt="The Fary pointing at you"
+                    src={'assets/img/' + item.picture}
+                    alt={item.alternativeText}
                 />
                 <span className="gentleman__initial">F</span>
             </div>
             <div className="gentleman__data-container">
-                <h2 className="gentleman__name">The Fary</h2>
+                <h2 className="gentleman__name">{item.name}</h2>
                 <ul className="gentleman__data-list">
                     <li className="gentleman__data">
                         <span className="gentleman__data-label">
                             Profession:
                         </span>
-                        Influencer
+                        {item.profession}
                     </li>
                     <li className="gentleman__data">
                         <span className="gentleman__data-label">Status:</span>{' '}
